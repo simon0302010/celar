d1a937130d61f5e788462811a744a8eb04244986
simon0302010 <simon0302010@gmail.com>
2025-09-29 13:21:21 +0200

    Added password hashing for database. Also added simple client for testing

--- client/main.py
+++ client/main.py
@@ -1,12 +1,19 @@
 import requests
+import sys
 
 API_URL = "http://127.0.0.1:8000"
 
 def register():
+    print("=== Register ===")
     username = input("Username: ")
     password = input("Password: ")
-    age = int(input("Age: "))
+    age = input("Age: ")
+    if not age.isnumeric():
+        print("Please input a number.")
+        sys.exit(1)
     interests = input("Interests (comma-separated): ").split(",")
+    for interest in interests:
+        interest.strip()
     r = requests.post(f"{API_URL}/register", json={
         "username": username,
         "password": password,
@@ -15,5 +22,19 @@ def register():
     })
     print(r.json())
 
+def login():
+    print("=== Login ===")
+    username = input("Username: ")
+    password = input("Password: ")
+    r = requests.post(f"{API_URL}/login", json={
+        "username": username,
+        "password": password
+    })
+    print(r.json())
+
 if __name__ == "__main__":
-    register()
+    answer = int(input("Press 1 to registers and press 2 to login: "))
+    if answer == 1:
+        register()
+    elif answer == 2:
+        login()
